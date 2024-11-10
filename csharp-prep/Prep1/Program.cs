2961c14f1a18767fa56af63520c68d2deb70a541
Sergio Varas <sergiovaras786@gmail.com>
2024-11-09 21:07:33 -0300

    new change

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("what is your first name? ");
+        Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
-        Console.Write("what is last name? ");
+        Console.Write("What is last name? ");
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
