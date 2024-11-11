347dabcb219381cb701808a16e6098cc80f725f8
Sergio Varas <sergiovaras786@gmail.com>
2024-11-11 16:12:33 -0300

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guessNumber = -1;
+        while (guessNumber != magicNumber)
+    
+    {
+        Console.Write("whta is your guess? ");
+        guessNumber = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guessNumber)
+
+
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");        }
+       }
     }
 }
