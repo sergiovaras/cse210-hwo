027a712e89d84fb4d355a6aeed6ea14413c686f6
Sergio Varas <sergiovaras786@gmail.com>
2024-12-03 17:27:28 -0300

    finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Samuel Benneett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assigment2 = new MathAssignment("Robert Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assigment2.GetSummary());
+        Console.WriteLine(assigment2.GetHomeworkList());
+
+
+        WritingAssignment assigment3 = new WritingAssignment("Mary Waters", "European History","The Causes of World War II by Mary Waters");
+        Console.WriteLine(assigment3.GetSummary());
+        Console.WriteLine(assigment3.GetWritingInformation());
     }
 }
---
+++ prepare/Learning05/WritingAssignment.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base (studentName,topic)
+    {
+        _title = title;
+    }
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetWritingInformation()
+    {
+        return _title;
+
+    }
+
+
+}
